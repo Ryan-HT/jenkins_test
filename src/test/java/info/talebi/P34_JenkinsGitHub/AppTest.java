4ecd87bd66e9afec73ef3fa436e138b9c1d2bad7
Ryan <Ryan@DESKTOP-0Q8QFF6>
2020-03-12 16:59:49 +0100

    Creating test

---
+++ src/test/java/info/talebi/P34_JenkinsGitHub/AppTest.java
@@ -0,0 +1,19 @@
+package info.talebi.P34_JenkinsGitHub;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		System.out.println("This is testing form JenkinsGitHub");
+	}
+
+}
